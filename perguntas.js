0c359b85b537ea05e508a794f8bc339481825f98
karlavalenca0 <kvalenca493@gmail.com>
2024-11-06 11:14:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'filme',
+    'qual a princesa do vestido verde ?',
+    'diana'
+)
+
+criaCartao(
+    'filmes',
+    'quantos personagens têm no divertida mente 2 ?',
+    'contem nove personagens'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'qual a tradução da frase:Rich soul strong mind ?',
+    'Alma rica mente forte'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'o que se retra a frase escrita em inglês do what you can, within the conditions you have?',
+    'faça o que você pode,Dentro das condições que você tem.'
+)
